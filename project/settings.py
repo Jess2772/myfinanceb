207cfcbc1a664134445f66bc82f3eba034448ae7
Jesse Li <jesse.li286@gmail.com>
2023-07-14 18:03:29 -0400

    I was supposed to do PD today

--- project/settings.py
+++ project/settings.py
@@ -40,11 +40,19 @@ CORS_ALLOWED_ORIGINS = [
     'https://incredible-caramel-35da02.netlify.app'
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'https://incredible-caramel-35da02.netlify.app'
-]
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = [
+  'accept',
+  'accept-encoding',
+  'authorization',
+  'content-type',
+  'origin',
+  'dnt',
+  'user-agent',
+  'x-csrftoken',
+  'x-requested-with']
+CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 # Application definition
 
 INSTALLED_APPS = [
