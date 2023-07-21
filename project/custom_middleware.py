e321408025bbb40179c2158072c4a8590543f144
Jesse Li <jesse.li286@gmail.com>
2023-07-20 23:26:34 -0400

    run dat

--- project/custom_middleware.py
+++ project/custom_middleware.py
@@ -4,5 +4,6 @@ class CsrfHeaderMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if "CSRF_COOKIE" in request.META:
             # csrfviewmiddleware sets response cookie as request.META['CSRF_COOKIE']
+            #response["Set-Cookie"]
             response["X-CSRFTOKEN"] = request.META['CSRF_COOKIE']
         return response
