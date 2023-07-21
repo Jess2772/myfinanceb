a81551f102827f11506944191aa88fac529e4411
Jesse Li <jesse.li286@gmail.com>
2023-07-20 23:48:29 -0400

    idk

--- project/custom_middleware.py
+++ project/custom_middleware.py
@@ -6,7 +6,8 @@ class CsrfHeaderMiddleware(MiddlewareMixin):
             # csrfviewmiddleware sets response cookie as request.META['CSRF_COOKIE']
             #response["Set-Cookie"]
             
-            response["Set-Cookie"] = request.headers['Cookie'].split(";")[0] + "; expires=Fri, 19 Jul 2024 03:43:33 GMT; Max-Age=31449600; Path=/; SameSite=None; Secure"
-            response["Set-Cookie"] = request.headers['Cookie'].split(";")[1] + "; expires=Fri, 19 Jul 2024 03:43:33 GMT; Max-Age=31449600; Path=/; SameSite=None; Secure"
+            a = request.headers['Cookie'].split(";")[0] + "; expires=Fri, 19 Jul 2024 03:43:33 GMT; Max-Age=31449600; Path=/; SameSite=None; Secure "
+            b = request.headers['Cookie'].split(";")[1] + "; expires=Fri, 19 Jul 2024 03:43:33 GMT; Max-Age=31449600; Path=/; SameSite=None; Secure"
+            response["Set-Cookie"] = a + b
             response["X-CSRFTOKEN"] = request.META['CSRF_COOKIE']
         return response
