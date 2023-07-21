4e675381e155ae0e8a9f8ed750a06335a3091117
Jesse Li <jesse.li286@gmail.com>
2023-07-20 23:18:35 -0400

    update path

--- project/settings.py
+++ project/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'custom_middleware.CsrfHeaderMiddleware',
+    'project.custom_middleware.CsrfHeaderMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
